b6e7dec62fe69c4ae3b0ec7af1f6376dff1b4c6d
porokiller2502@gmail.com <porokiller2502@gmail.com>
2020-05-27 09:05:26 +0700

    Add project files.

---
+++ eShopS.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopS.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
